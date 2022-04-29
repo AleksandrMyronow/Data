e4d277df6eabd3a84ca090f84432e2e709c9ee87
AleksandrMyronow <a.myronow@gmail.com>
2022-04-29 22:54:46 +0300

    Second chance init

---
+++ Python/Corsea/1/1.py
@@ -0,0 +1,3 @@
+user_name = input ('Please enter your name: ');
+user_city = input ('Ok ' + user_name + ' where are you from: ');
+print ('Nice to meet you ' + user_name + ' from ' + user_city);
---
+++ Python/Haudi/1.py
@@ -0,0 +1,17 @@
+##age = int(input ('How old are you?: '));
+##if age >= 18:
+##    country = str(input('Where are you from? '));
+##    if country == 'Ukraine' or country == 'USA' and age >= 18:
+##        print('Welcome on board baby');
+##    else:
+##        print ('You cant watch this content');
+##else: 
+##    print ('you are too young')
+
+#i = 3;
+#while i >= 0:
+#    print(i);
+#    i=i-1
+
+import calendar
+print (calendar.month(2022,4))
